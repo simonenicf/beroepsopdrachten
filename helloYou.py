ae8ab8c4396bea7e75c945147540fe4ade6ae586
michelle frankfort <j.s.frankfort@gmail.com>
2020-09-09 12:19:00 +0200

    added if, elif and else statment

--- helloYou.py
+++ helloYou.py
@@ -5,3 +5,10 @@ print("username is: " + username)
 import datetime
 x = datetime.datetime.now()
 print(x)
+answer = input(username + ", wil jij dit programma nog een keer doen. Type Y/N ").lower()
+if answer == "Y"or answer == "y":
+       	print('Hooray, I can!')
+elif answer == "N" or answer == "n" :
+      	print('Thank you, ' + username)
+else:
+       	print('Huh?')
